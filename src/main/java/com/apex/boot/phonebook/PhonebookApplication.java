6b894bbbac9624e53bd74a65f71a3c4d77b01a9b
alidad <alidad@dorisa.com>
2020-09-13 16:20:08 -0700

    initial save

---
+++ src/main/java/com/apex/boot/phonebook/PhonebookApplication.java
@@ -0,0 +1,13 @@
+package com.apex.boot.phonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonebookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apex/boot/phonebook/PhonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.boot.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
